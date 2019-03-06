edebe9d75359e18a347340645dc0213038f3a2d1
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-03-06 17:10:51 +0000

    Added if/else for databases in settings.py for travis integration to work

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 # Importing env.py allows you to use your environment variables set there
-import env
+# import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -84,14 +84,18 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
+if "DATABASE_URL" in os.environ: # Use the cloud database
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) 
+    }
+else: # Use the local database
+    print("Database URL not found. Using SQLite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
